8dc5720853f1f5d70b3f090044a033a5beafdc44
BuildTools <unconfigured@null.spigotmc.org>
2021-04-28 13:55:17 +0200

    test1

--- ifgkjfujfgufgulfg.py
+++ ifgkjfujfgufgulfg.py
@@ -1,1 +1,40 @@
-print("ha")
+cisla = [1, 2, 9, 3, 5, 8]
+cisla.sort(reverse=True)
+print(cisla)
+
+def serazovac(*cisla):
+    cisla = list(cisla)
+    cisla.sort(reverse=True)
+    print(cisla)
+
+serazovac(1,8,9,5)
+
+
+
+
+
+class Cisla:
+    def __init__(self, cislo, definice):
+        self.cislo = cislo
+        self.definice = definice
+
+
+    def vypis(self):
+        print("Číslo je: " + str(self.cislo) + " a je " + self.definice)
+
+
+dvojka = Cisla(2, "celé")
+
+
+class CelaCisla(Cisla):
+    def __init__(self, cislo, definice, delitelnost):
+        super().__init__(cislo, definice)
+        self.delitelnost = delitelnost
+
+
+
+dvojka.vypis()
+
+
+
+
